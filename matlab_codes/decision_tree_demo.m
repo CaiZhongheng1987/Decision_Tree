8a12da29495498983f02745039eaef17554d0e9a
CaiZhongheng1987 <38467143+CaiZhongheng1987@users.noreply.github.com>
2018-07-15 15:18:05 +0800

    决策树运行主脚本

---
+++ matlab_codes/decision_tree_demo.m
@@ -0,0 +1,35 @@
+% name   :       decision tree demo
+% author :       CaiZhongheng
+ 
+% date           version          record
+% 2018.07.15     v1.0             init
+
+
+% setting
+create_method = 0; % 0: ID3, 1: C4.5 2: least_square 3: CART
+
+
+% traing data
+
+
+% age                  0: young, 1: middle aged, 2: old 
+feature_matrix(1,:) = [0 0 0 0 0 1 1 1 1 1 2 2 2 2 2];
+% work status          0: no job, 1: have job
+feature_matrix(2,:) = [0 0 1 1 0 0 0 1 0 0 0 0 1 1 0];
+% house status         0: no house 1: have house
+feature_matrix(3,:) = [0 0 1 1 0 0 0 1 0 0 0 0 1 1 0];
+% loan status before   0: just so so, 1: good, 2: excellent
+feature_matrix(4,:) = [0 1 1 0 0 0 1 1 2 2 2 1 1 2 0];
+%                      0: do not give loan, 1: give loan. 
+class_matrix        = [0 0 1 1 0 0 0 1 1 1 1 1 1 1 0];
+
+feature_num         = size(feature_matrix,1);
+len_training_data   = size(feature_matrix,2);
+
+
+
+
+% test data
+
+
+
